de265d0fa33757a4e0b517d3e0d456ffaa0ad6dc
almazfarhatovich <janybekovalmaz29@gmail.com>
2022-03-09 20:20:53 +0600

    новый вариант

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,13 +19,13 @@ public class Main {
         set2.add(0);
         set2.add(1);
         set2.add(2);
-        symmetricDifference(set1, set2);
+        System.out.println(symmetricDifference(set1, set2));
     }
-    public static void symmetricDifference(HashSet<Integer>set1, HashSet<Integer>set2){
+    public static Set symmetricDifference(HashSet<Integer>set1, HashSet<Integer>set2){
         HashSet<Integer> newSet = new HashSet<Integer>(set1);
         newSet.removeAll(set2);
         set2.removeAll(set1);
         newSet.addAll(set2);
-        System.out.println(newSet);
+        return newSet;
     }
 }
